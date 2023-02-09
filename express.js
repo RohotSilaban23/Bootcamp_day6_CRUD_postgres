520e7294eca456c8f85557633d7ee232754173f7
RohotSilaban23 <rohotsilaban9@gmail.com>
2023-02-09 11:15:10 +0700

    mengirim data array melalui router

--- express.js
+++ express.js
@@ -2,16 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.sendFile('./index.html',{root: __dirname})
+  // res.sendFile('./index.html',{root: __dirname})
+  res.render('index')
 })
 
 app.get('/abaut', (req, res) => {
-  res.send('./abaut.html',{root: __dirname})
+  // res.send('./abaut.html',{root: __dirname})
+  res.render('abaut')
 })
 
 app.get('/contact', (req, res) => {
-  res.send('./conatct.html',{root: __dirname})
+  // res.send('./conatct.html',{root: __dirname})
+  const data= [{nama : 'Rohot', noTelpon: '082299008023'}]
+  
+  res.render('contact', {data:data})
+
 })
 
 
@@ -19,7 +27,7 @@ app.get('/product/:Idproduct/kategori/:name', (req, res) => {
   const id = req.params.Idproduct
   const name = req.params.name
   
-  res.send(`Id product  ${id}, \n Nama Kategori : ${name}`)
+  res.send(`Id product  ${id}, <br> Nama Kategori : ${name}`)
 })
 
 app.use('/', (req, res) => {
